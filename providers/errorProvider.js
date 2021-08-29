c4d4d58431b0af55350f64dc17d7f5d173121604
cirine01 <sdimassi01@gmail.com>
2021-08-29 22:02:20 +0200

    Delete providers directory

--- providers/errorProvider.js
+++
@@ -1,8 +0,0 @@
-export const ErrorCatcher = error => {
-    if (error.status === 498 || error.status === 401) {
-        console.warn("Error detected. Token is invalid, logout incoming.");
-        if (window.setOpenTokenError) {
-            window.setOpenTokenError(true);
-        }
-    }
-};
