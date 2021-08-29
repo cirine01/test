72e53f2dab3e1c05b5f041016ed0d022a12dd139
cirine01 <sdimassi01@gmail.com>
2021-08-29 22:01:57 +0200

    Delete components directory

--- components/EsopNotification.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Notification } from "react-admin";
-
-const EsopNotification = props => <Notification {...props} autoHideDuration={null} />;
-
-export default EsopNotification;
